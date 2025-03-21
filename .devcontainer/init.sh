e057bb9400d50a1b130f8673eb04c9428d63e590
Achille Lacoin <achille.lacoin@gmail.com>
2025-03-21 10:53:58 +0000

    add smallweb sync logic

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "$SSH_PRIVATE_KEY" > /vscode/.ssh/id_ed25519
-chmod 600 /vscode/.ssh/id_ed25519
+mkdir -p ~/.ssh
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519 && chmod 600 ~/.ssh/id_ed25519
+
+cat <<EOF > ~/.ssh/config && chmod 644 ~/.ssh/config
+Host vps
+    HostName 162.55.213.87
+    Port 2222
+    User pomdtr
+    LogLevel ERROR
+    StrictHostKeyChecking no
+EOF
