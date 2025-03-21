39ac0eab913e353fd4df4445c2ab895fa8533412
Achille Lacoin <achille.lacoin@gmail.com>
2025-03-21 12:17:59 +0000

    install sops in devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519 && chmod 600 ~/.ssh/id_ed25519
+echo -n "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519 && chmod 600 ~/.ssh/id_ed25519
 cp .devcontainer/ssh_config ~/.ssh/config && chmod 644 ~/.ssh/config
